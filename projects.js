9331cff7d27b2ac1da0ae413a83879fa9516eb10
Suhana783 <suhanachauhan133@gmail.com>
2025-09-25 12:03:30 +0530

    building portfolio

---
+++ projects.js
@@ -0,0 +1,8 @@
+let projects = document.getElementById("projects");
+let projectGrid = document.querySelector(".project-grid");
+
+projectGrid.addEventListener("mouseover", function () {
+  let id = setInterval(()=>{
+    projectGrid = "";},9000);
+  projects.style.display = "grid";
+});
